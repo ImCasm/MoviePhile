4fb9be3283ae3dc522a77b6d0b858bafb99c9c4c
Cristian Sánchez <cristian.asm19@gmail.com>
2021-07-15 19:55:41 -0500

    Se agrega migración para aficionar la columna Title a la tabla de Publication

--- Domain/Entities/Publication.cs
+++ Domain/Entities/Publication.cs
@@ -6,14 +6,13 @@ namespace Domain.Entities
     public class Publication
     {
         public int Id { get; set; }
+        public string Title { get; set; }
         public string Content { get; set; }
         public DateTime Date { get; set; }
         public string UserId { get; set; }
         public int CommunityId { get; set; }
-
         public ICollection<PublicationComment> Comments { get; set; }
         public Community Community { get; set; }
         public User User { get; set; }
-
     }
 }
--- Persistence/Migrations/20210716004823_addPostTitle.Designer.cs
+++ Persistence/Migrations/20210716004823_addPostTitle.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Persistence;
 
 namespace Persistence.Migrations
 {
     [DbContext(typeof(MoviePhileDbContext))]
-    partial class MoviePhileDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210716004823_addPostTitle")]
+    partial class addPostTitle
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

a02c5c87851e2164f0dfeb1d116869c59312cfe2
Esteban Alvarez <johndoe@example.com>
2021-07-20 11:56:08 -0500

    Implementación  pruebas unitarias de postear en comunidad

---
+++ Tests/UnitTests/PublicationCommunity/Mocks/PublicationCommunityRepositoryMock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Application.Common.Interfaces.Repository;
+using Moq;
+using Domain.Entities;
+
+namespace Tests.UnitTests.PublicationCommunity.Mocks
+{
+    class PublicationCommunityRepositoryMock : Mock<IPublicationRepository>
+    {
+        public PublicationCommunityRepositoryMock()
+        {
+            SetupMethods();
+        }
+
+        public void SetupSetPublication()
+        {
+
+          
+            Setup(x => x.SetPublication(It.Is<Publication>(publicationCommunity => string.IsNullOrEmpty(publicationCommunity.Content))))
+                .Returns(Task.FromResult(false));
+            
+            
+            Setup(x => x.SetPublication(It.Is<Publication>(publicationCommunity => !string.IsNullOrEmpty(publicationCommunity.Content))))
+                .Returns(Task.FromResult(true));
+            
+            Setup(x => x.SetPublication(It.Is<Publication>(publicationCommunity => string.IsNullOrEmpty(publicationCommunity.UserId))))
+               .Returns(Task.FromResult(false));
+            
+        }
+
+        private void SetupMethods()
+        {
+            SetupSetPublication();
+        }
+    }
+}

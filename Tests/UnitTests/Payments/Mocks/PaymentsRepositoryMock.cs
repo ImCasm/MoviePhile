a8c871b7e2a5fcec02428305fcd1af8882df7ce3
Cristian Sánchez <cristian.asm19@gmail.com>
2021-07-31 17:17:26 -0500

    Se relizan las pruebas del item K2 - Registrar pago

---
+++ Tests/UnitTests/Payments/Mocks/PaymentsRepositoryMock.cs
@@ -0,0 +1,21 @@
+﻿using Application.Common.Interfaces.Repository;
+using Domain.Entities;
+using Moq;
+using System.Threading.Tasks;
+
+namespace Tests.UnitTests.Payments.Mocks
+{
+    public class PaymentsRepositoryMock : Mock<IPaymentsRepository>
+    {
+        public PaymentsRepositoryMock()
+        {
+            SetupCreatePayment();
+        }
+
+        public void SetupCreatePayment()
+        {
+            Setup(x => x.CreatePayment(It.IsAny<Payment>()))
+            .Returns(Task.FromResult(1023));
+        }
+    }
+}
---
+++ Tests/UnitTests/Payments/PaymentShould.cs
@@ -0,0 +1,38 @@
+﻿using Application.Common.Interfaces.Services;
+using Application.Services;
+using Domain.Entities;
+using System;
+using Tests.UnitTests.Payments.Mocks;
+using Xunit;
+
+namespace Tests.UnitTests.Payments
+{
+    public class PaymentShould
+    {
+        private readonly PaymentsRepositoryMock _paymentsRepository;
+        private readonly IPaymentsService _paymentService;
+
+        public PaymentShould()
+        {
+            _paymentsRepository = new PaymentsRepositoryMock();
+            _paymentService = new PaymentsTestService(_paymentsRepository.Object);
+        }
+
+        [Fact]
+        public async void CreatePayment()
+        {
+            // Arrange
+            Payment payment = new Payment()
+            {
+                Amount = 3000,
+                Date = DateTime.UtcNow,
+            };
+
+            // Act
+            int id = await _paymentService.CreatePayment(payment);
+
+            //Assert
+            Assert.True(id > 0);
+        }
+    }
+}
